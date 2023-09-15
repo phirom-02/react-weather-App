3982cdd4ad2471b69378c114e9692fd122df8740
phirom-02 <phiromkhim02@gmail.com>
2023-09-15 14:55:12 +0700

    feature: Create helper and other util function.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,12 +1,13 @@
+import icons from './icons';
+
 /**
- * This function will convert unix time to human readable time.
- * @param {int} dt
+ * This function will convert unix date to human readable date.
+ * @param {int} unixTimestamp
  * @returns {obj}
  */
-export const convertUnixTimeStamp = (dt) => {
-	const date = new Date(dt * 1000);
+export const getDate = (unixTimestamp) => {
+	const date = new Date();
 
-	// This function will convert interger to day of week
 	const dayOfWeek = [
 		'Sunday',
 		'Monday',
@@ -17,11 +18,52 @@ export const convertUnixTimeStamp = (dt) => {
 		'Saturday',
 	];
 
+	const monthOfYear = [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dec',
+	];
+
 	return {
-		month: date.getMonth(),
+		month: monthOfYear[date.getMonth()],
 		day: date.getDate(),
 		dayOfWeek: dayOfWeek[date.getDay()],
-		hours: date.getHours(),
-		minutes: date.getMinutes(),
 	};
+};
+
+/**
+ * This function will convert unix time to human readable time.
+ * @param {int} unixTimestamp
+ * @param {int} timeZoneOffset
+ * @returns {int}
+ */
+export const unixTimestampToHours = (unixTimestamp, timeZoneOffset) => {
+	const timestampInSeconds = unixTimestamp + timeZoneOffset;
+	const hours = Math.floor(timestampInSeconds / 3600);
+	const minutes = Math.floor((timestampInSeconds / 60) % 60);
+
+	// determine whether it's AM or PM
+	const period = hours % 24 < 12 ? 'AM' : 'PM';
+
+	// convert 24-hour time format to 12-hour time format
+	const formattedHour = hours % 12 || 12;
+	return { formattedHour, minutes, period };
+};
+
+/**
+ * Return icon path based on iconCode
+ * @param {string} iconCode
+ * @returns {icon}
+ */
+export const getIcon = (iconCode) => {
+	return icons[iconCode];
 };
---
+++ src/utils/icons.js
@@ -0,0 +1,33 @@
+import clearDay from './../assets/01d.svg';
+import clearNight from './../assets/01n.svg';
+import fewCloudsDay from './../assets/02d.svg';
+import fewCloudsNight from './../assets/02n.svg';
+import scatteredClouds from './../assets/03dn.svg';
+import brokenClouds from './../assets/04dn.svg';
+import showerRain from './../assets/09dn.svg';
+import rainDay from './../assets/10d.svg';
+import rainNight from './../assets/10n.svg';
+import thunderstorm from './../assets/11dn.svg';
+import snow from './../assets/13dn.svg';
+import mist from './../assets/50dn.svg';
+
+export default {
+	'01d': clearDay,
+	'01n': clearNight,
+	'02d': fewCloudsDay,
+	'02n': fewCloudsNight,
+	'03d': scatteredClouds,
+	'03n': scatteredClouds,
+	'04d': brokenClouds,
+	'04n': brokenClouds,
+	'09d': showerRain,
+	'09n': showerRain,
+	'10d': rainDay,
+	'10n': rainNight,
+	'11d': thunderstorm,
+	'11n': thunderstorm,
+	'13d': snow,
+	'13n': snow,
+	'50d': mist,
+	'50n': mist,
+};
